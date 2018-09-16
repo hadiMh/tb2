e9d518c154f53b45caab6de78ef236243d9f38b1
HadiMh <hadih7899@gmail.com>
2018-09-16 11:19:41 +0430

    some changes

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = mysqli_connect(getenv('BOT2DBSERVER'), getenv('BOT2DBUSERNAME'), getenv('BOT2DBPASSWORD'), getenv('BOT2DBTABLE'), '3306');
+    $connection = mysqli_connect(getenv('BOT2DBSERVER'), getenv('BOT2DBUSERNAME'), getenv('BOT2DBPASSWORD'), getenv('BOT2DBNAME'), '3306');
     // var_dump(function_exists('mysqli_connect'));
     if(!$connection) {
         echo "Database connection failed. shittt!";
