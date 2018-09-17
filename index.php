c03eb7c4dd90800bdfcb5742f66033fc2a620d2d
HadiMh <hadih7899@gmail.com>
2018-09-17 22:57:00 +0430

    test sending message

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     $userBotStatus = getUserColumnData($userId, "bot_status");
     if($userBotStatus === '1') {
         sendMessage($chatId, "پیام شما با موفقیت ارسال شد. در اولین فرصت بررسی شده و به شما اطلاع داده میشود.", returnEM($main_panel));
-        sendMessage(151553837, $message, returnEMhide());
+        sendMessage(151553837, "کاربر @$username پیامی ارسال کرده است:".$message, returnEMhide());
     }
 
     if($message === "/start") {
