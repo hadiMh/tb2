ed9bde01bbdf9b9afa5db1a5b7d211ecb27cefe2
HadiMh <hadih7899@gmail.com>
2018-09-17 19:44:37 +0430

    test bug

--- db.php
+++ db.php
@@ -2,5 +2,5 @@
     $connection = mysqli_connect(getenv('BOT2DBSERVER'), getenv('BOT2DBUSERNAME'), getenv('BOT2DBPASSWORD'), getenv('BOT2DBNAME'), '3306');
     // var_dump(function_exists('mysqli_connect'));
     if(!$connection) {
-        "database connection failed!";
+        
     }
