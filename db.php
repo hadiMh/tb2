77bfa3cdd8b8a94733892dacd72c1ff2aa1bfe7c
HadiMh <hadih7899@gmail.com>
2018-09-17 19:25:35 +0430

    test bug

--- db.php
+++ db.php
@@ -1,6 +1,14 @@
 <?php
+function sendMessage($chatId, $message, $r)
+{
+    $url = $GLOBALS['webSite'] . "/sendMessage?chat_id=" . $chatId . "&text=" . urlencode($message) . "&reply_markup=" . $r;
+    file_get_contents($url);
+}
+
     $connection = mysqli_connect(getenv('BOT2DBSERVER'), getenv('BOT2DBUSERNAME'), getenv('BOT2DBPASSWORD'), getenv('BOT2DBNAME'), '3306');
     // var_dump(function_exists('mysqli_connect'));
     if(!$connection) {
-        echo "database connection failed!";
+        sendMessage($chatId, "database connection failed!");
+    }else {
+        sendMessage($chatId, "database connection successful!");
     }
