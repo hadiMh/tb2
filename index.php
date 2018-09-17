b439cf0a7ef2b0ec12be10e28c4bf05c360ec4b3
HadiMh <hadih7899@gmail.com>
2018-09-17 19:05:59 +0430

    bug fix

--- index.php
+++ index.php
@@ -27,9 +27,9 @@
     sendMessage($chatId, "-1", returnEMhide());
     if($message === "/start") {
         sendMessageToCurrentUser("به ربات ثبت شکایت و پیشنهاد خوش آمدید.\n این ربات برای ارسال تمامی مشکلات، پیشنهادات و هر مورد ارتباط با بخش معاونت آموزشی دانشگاه ساخته شده است تا تمامی این موارد در کمترین زمان ممکن به مسئولین مربوطه ازسال شود و هر گونه نارضایتی و مشکلی را در کمترین زمان ممکن حل کند.\nبرای استفاده ز ربات میتوانید از دکمه های زیر استفاده کنید.",$main_panel);
-    } /* else if($message == $main_panel[0][0]) {
+    } else if($message == $main_panel[0][0]) {
         sendMessageToCurrentUser("$main_panel[0][0] was clicked.");
-    } */
+    }
 
     // sendMessageReplyTo($chatId, "hello", returnEMhide(), $messageId);
 
