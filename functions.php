02c935b6f751806f571cbca95e1b5a4fa119ba46
HadiMh <hadih7899@gmail.com>
2018-09-18 11:22:29 +0430

    Revert "fixing query design"
    This reverts commit 893fda6d41632a55eeaf273033d6bb1192b51ce1.

--- functions.php
+++ functions.php
@@ -128,20 +128,19 @@
         global $connection;
 
         $now_date_time = date("Y-m-d H:i:s");
-        $sampleJson = '
+        $sampleJson = "
             {
-                "text" : "$msgText",
-                "date" : "$now_date_time",
-                "status" : "0"
+                'text' : '$msgText',
+                'date' : '$now_date_time',
+                'status' : '0'
             }
-        '; 
+        "; 
 
         $msgsJson = getUserMsg($userId);
         $msgsJson = substr($msgsJson, 0, -1);
-        if($msgsJson === "[]"){} else
-            $msgsJson .= ",";
         $msgsJson .= $sampleJson;
-        $msgsJson .= "]";
+        $msgsJson .= ",]";
+        $msgsJson = str_replace(" ","",$msgsJson);
         $msgsJson = str_replace("\n","",$msgsJson);
         $query = "UPDATE users SET ";
         $query .= "msgs = \"".$msgsJson."\" ";
