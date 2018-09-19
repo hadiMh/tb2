95e97fec7f4ecf701bd5b09f964462caa7d9a63d
HadiMh <hadih7899@gmail.com>
2018-09-19 13:16:05 +0430

    fixing utf8 problem

--- db.php
+++ db.php
@@ -1,5 +1,6 @@
 <?php
     $connection = mysqli_connect(getenv('BOT2DBSERVER'), getenv('BOT2DBUSERNAME'), getenv('BOT2DBPASSWORD'), getenv('BOT2DBNAME'), '3306');
+    $connection->set_charset("utf8");
     if(!$connection) {
         // should log it
     }
